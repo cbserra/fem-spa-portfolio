0d28d4cf804153d66f66ef946100f8c8ac2f9c58
cbserra <789723+cbserra@users.noreply.github.com>
2022-07-18 10:09:48 -0700

    chore: added linters

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,25 +3,30 @@
 module.exports = {
   content: ["index.html", "./src/**/*.{js,jsx,ts,tsx,vue,html}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        lg: "1440px",
+      },
+    },
     colors: {
       "galactic-blue": "hsl(252, 66%, 62%)",
-      'summer-yellow': "hsl(28, 89%, 67%)",
-      'pink': "hsl(0, 78%, 79%)",
-      'light-red': "hsl(7, 77%, 66%)",
-      'cyan': "hsl(172, 46%, 57%)",
-      'burgundy': "hsl(314, 45%, 23%)",
-      'black': "hsl(0, 0%, 1%)",
+      "summer-yellow": "hsl(28, 89%, 67%)",
+      pink: "hsl(0, 78%, 79%)",
+      "light-red": "hsl(7, 77%, 66%)",
+      cyan: "hsl(172, 46%, 57%)",
+      burgundy: "hsl(314, 45%, 23%)",
+      black: "hsl(0, 0%, 1%)",
       "medium-brown": "hsl(30, 5%, 45%)",
       "light-cream": "hsl(28, 100%, 97%)",
+      transparent: "transparent",
     },
     backgroundImage: {
-      'graphic-design': 'url("./pattern-graphic-design.svg")',
-      'illustrations': 'url("./pattern-illustrations.svg")',
-      'apps': 'url("./pattern-apps.svg")',
-      'motion-graphics': 'url("./pattern-motion-graphics.svg")',
-      'photography': 'url("./pattern-photography.svg")',
-      'ui-ux': 'url("./pattern-ui-ux.svg")',
+      "graphic-design": 'url("./pattern-graphic-design.svg")',
+      illustrations: 'url("./pattern-illustrations.svg")',
+      apps: 'url("./pattern-apps.svg")',
+      "motion-graphics": 'url("./pattern-motion-graphics.svg")',
+      photography: 'url("./pattern-photography.svg")',
+      "ui-ux": 'url("./pattern-ui-ux.svg")',
     },
     fontFamily: {
       sans: ["Plus Jakarta Sans", "sans-serif"],
@@ -40,19 +45,22 @@ module.exports = {
         },
       ],
       sm: [
-         '14px', {
-          lineHeight: '28px',
-         }
+        "14px",
+        {
+          lineHeight: "28px",
+        },
       ],
       md: [
-        '24px', {
-          lineHeight: '30px',
-        }
+        "24px",
+        {
+          lineHeight: "30px",
+        },
       ],
       lg: [
-        '26px', {
-          lineHeight: '33px',
-        }
+        "26px",
+        {
+          lineHeight: "33px",
+        },
       ],
     },
   },
