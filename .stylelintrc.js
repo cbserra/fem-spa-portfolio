82cc1a81f2ec1492f1487b6c71169c40e25d0e01
cbserra <789723+cbserra@users.noreply.github.com>
2022-07-20 06:08:24 -0700

    build(lint): lint configs updated linter configs

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,22 +1,23 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-prettier/recommended"],
-  plugins: ["stylelint-prettier"],
-  rules: {
-    "selector-no-qualifying-type": [
-      true,
-      {
-        ignore: ["attribute", "class"],
-      },
-    ],
-    "selector-max-id": [2],
-    "alpha-value-notation": "percentage",
-    "prettier/prettier": true,
-    "at-rule-no-unknown": [
-      true,
-      {
-        ignoreAtRules: ["tailwind", "apply", "variants", "responsive"],
-      },
-    ],
-    "selector-class-pattern": "^[a-zA-Z][a-zA-Z0-9-_]*$",
-  },
+  "extends": ["stylelint-prettier/recommended"]
+  // extends: ["stylelint-config-standard", "stylelint-prettier/recommended"],
+  // plugins: ["stylelint-prettier"],
+  // rules: {
+  //   "selector-no-qualifying-type": [
+  //     true,
+  //     {
+  //       ignore: ["attribute", "class"],
+  //     },
+  //   ],
+  //   "selector-max-id": [2],
+  //   "alpha-value-notation": "percentage",
+  //   "prettier/prettier": true,
+  //   "at-rule-no-unknown": [
+  //     true,
+  //     {
+  //       ignoreAtRules: ["tailwind", "apply", "variants", "responsive"],
+  //     },
+  //   ],
+  //   "selector-class-pattern": "^[a-zA-Z][a-zA-Z0-9-_]*$",
+  // },
 };
